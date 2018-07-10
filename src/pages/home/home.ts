47719b0d8a3f467747bdaedd70254c3fe53fa86b
Mani Kumar K V <mkanamarlapudi@nasinsurance.com>
2018-07-10 22:04:05 +0530

    git

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { NavController , LoadingController } from "ionic-angular";
+import { NavController, LoadingController } from "ionic-angular";
 import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 import { HttpClient } from "@angular/common/http";
 
@@ -8,9 +8,9 @@ import { HttpClient } from "@angular/common/http";
   templateUrl: "home.html"
 })
 export class HomePage {
-  barCodeData = null
-  data: Array<any> = []; 
-  testData: string ='' 
+  barCodeData = null;
+  data: Array<any> = [];
+  testData: string = "";
   constructor(
     public navCtrl: NavController,
     private barcodeScanner: BarcodeScanner,
@@ -22,18 +22,17 @@ export class HomePage {
       .scan()
       .then(barcodeData => {
         this.barCodeData = barcodeData;
-        console.log(JSON.stringify(barcodeData))
+        console.log(JSON.stringify(barcodeData));
         let loading = this.loadingCtrl.create({
-          content: 'Loading VIN Info...'
+          content: "Loading VIN Info..."
         });
         loading.present();
         this.getVehicleDetails(barcodeData.text).subscribe(response => {
           loading.dismiss();
-          console.log('success')
-          this.testData = JSON.stringify(response)
-          console.log('message',response.Message)
-          console.log('data here',response.data)
-          this.data = this.filterResponse(response.Results);
+          console.log("success");
+          this.testData = JSON.stringify(response);
+          
+          this.data = this.filterResponse(response); /// TS_IGNORE
         });
       })
       .catch(err => {
@@ -50,17 +49,17 @@ export class HomePage {
   }
   getVehicleDetails(vinNumber) {
     const url = this.geturl(vinNumber);
-    console.log(url)
+    console.log(url);
     return this.http.get(url);
   }
   filterResponse(result) {
-    console.log('result in')
+    console.log("result in");
     let resultData = [];
     const paramIds = [26, 27, 28, 29, 39];
-    resultData = result.filter(param => {
+    resultData = result.Results.filter(param => {
       return paramIds.indexOf(param.VariableId) > -1;
     });
-    console.log(JSON.stringify(resultData))
+    console.log(JSON.stringify(resultData));
     return resultData;
   }
 }
